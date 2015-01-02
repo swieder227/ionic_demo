1201061d506d34e91f74a337b375228a20e3c4fc
Seth Wieder <swieder227@gmail.com>
2015-01-02 01:11:31 -0500

    added rsvps ng-repeat, ngclick for social media, 1st pass at css styles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,33 @@
 angular.module('starter.controllers', [])
 
-.controller('PrerollCtrl', function($scope) {})
+.controller('PrerollCtrl', function($scope, RSVPS) {
+	$scope.rsvps = shuffle(RSVPS.all());
+
+	// Shuffle algorithm
+	// https://github.com/coolaj86/knuth-shuffle
+	function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
+
+	$scope.facebookEnabled = false;
+	$scope.twitterEnabled = false;
+
+})
 
 // .controller('DashCtrl', function($scope) {})
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,82 @@
 angular.module('starter.services', [])
 
+.factory('RSVPS', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var rsvps = [{
+    id: 0,
+    name: '0 Lorem Ipsum',
+    face: 'http://lorempixel.com/125/100/people'
+  }, {
+    id: 1,
+    name: '1 Lorem Ipsum',
+    face: 'http://lorempixel.com/126/100/people'
+  }, {
+    id: 2,
+    name: '2 Lorem Ipsum',
+    face: 'http://lorempixel.com/127/100/people'
+  }, {
+    id: 3,
+    name: '3 Lorem Ipsum',
+    face: 'http://lorempixel.com/128/100/people'
+  }, {
+    id: 4,
+    name: '4 Lorem Ipsum',
+    face: 'http://lorempixel.com/129/100/people'
+  }, {
+    id: 5,
+    name: '5 Lorem Ipsum',
+    face: 'http://lorempixel.com/130/100/people'
+  }, {
+    id: 6,
+    name: '6 Lorem Ipsum',
+    face: 'http://lorempixel.com/131/100/people'
+  }, {
+    id: 7,
+    name: '7 Lorem Ipsum',
+    face: 'http://lorempixel.com/132/100/people'
+  }, {
+    id: 8,
+    name: '8 Lorem Ipsum',
+    face: 'http://lorempixel.com/133/100/people'
+  }, {
+    id: 9,
+    name: '9 Lorem Ipsum',
+    face: 'http://lorempixel.com/134/100/people'
+  }, {
+    id: 10,
+    name: '10 Lorem Ipsum',
+    face: 'http://lorempixel.com/135/100/people'
+  }, {
+    id: 11,
+    name: '11 Lorem Ipsum',
+    face: 'http://lorempixel.com/136/100/people'
+  }, {
+    id: 12,
+    name: '12 Lorem Ipsum',
+    face: 'http://lorempixel.com/137/100/people'
+  }];
+
+  return {
+    all: function() {
+      return rsvps;
+    },
+    remove: function(rsvp) {
+      rsvps.splice(rsvps.indexOf(rsvp), 1);
+    },
+    get: function(id) {
+      for (var i = 0; i < chats.length; i++) {
+        if (rsvps[i].id === parseInt(id)) {
+          return rsvps[i];
+        }
+      }
+      return null;
+    }
+  }
+})
+
+
 // .factory('Chats', function() {
 //   // Might use a resource here that returns a JSON array
 
